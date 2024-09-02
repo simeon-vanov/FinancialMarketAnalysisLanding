9f49aa943856dcd35de858382a7d023bd1ef31d2
Simeon Vanov <simooovanov@gmail.com>
2024-09-02 20:25:17 +0300

    remove unnecessary files and add metadata content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,16 @@ const Overview: NextPage = () => {
     <>
       <Head>
         <title>Decode The Trade</title>
+        <meta property='og:title' content='Decode The Trade' />
+        <meta
+          property='og:description'
+          content='Decode The Trade is a platform for analyzing markets, identifying profitable setups and becoming a world-class trader.'
+        />
+        <meta property='og:url' content='https://decodethetrade.com' />
+        <meta property='og:image' content='https://decodethetrade.com/static/thumbnail.png' />
+        <meta property='og:type' content='website' />
+        <meta property='og:image:width' content='1280' />
+        <meta property='og:image:height' content='720' />
       </Head>
       <Hero />
       <Box>
